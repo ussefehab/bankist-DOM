f6c827de098eaa49fbeb9ab5e4da1334a98c4d6f
ussefehab <ussefehab@gmail.com>
2024-05-01 06:17:05 +0300

    menu fade annimation

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const section1 = document.querySelector('#section--1');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const navLinks = document.querySelector('.nav__links');
+const nav = document.querySelector('.nav');
 
 
 
@@ -46,3 +47,31 @@ navLinks.addEventListener('click',function(e){
     document.querySelector(id).scrollIntoView({behavior:'smooth'});
   }
 });
+
+//menu fade animation
+nav.addEventListener('mouseover',function(e){
+  if (e.target.classList.contains('nav__link')){
+    const link = e.target;
+    const siblings =link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+    siblings.forEach(el=>{
+      if (el !== link) el.style.opacity = 0.5;
+    });
+    logo.style.opacity = 0.5;
+
+  
+  }
+})
+nav.addEventListener('mouseout',function(e){
+  if (e.target.classList.contains('nav__link')){
+    const link = e.target;
+    const siblings =link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+    siblings.forEach(el=>{
+      if (el !== link) el.style.opacity = 1;
+    });
+    logo.style.opacity = 1;
+
+  
+  }
+})
