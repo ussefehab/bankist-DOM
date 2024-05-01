f1e2b4ad13ab2d433571ed1d0fc420ce9c68fb17
ussefehab <ussefehab@gmail.com>
2024-05-01 08:14:25 +0300

    keydown and arrow control

--- script.js
+++ script.js
@@ -190,7 +190,18 @@ const slider=function(){
 
   btnRight.addEventListener('click',nextSlide);
   btnLeft.addEventListener('click',prevSlide); 
-  
+  document.addEventListener('keydown',function (e) {
+    e.key === 'ArrowLeft' && prevSlide();
+    e.key === 'ArrowRight' && nextSlide();
+  })
+  dotContainer.addEventListener('click',function (e) {
+    if(    e.target.classList.contains('dots__dot')
+    ){
+  const {slide} = e.target.dataset;
+  goToSlide(slide);
+  activateDot(slide);
+    }
+  })
 
 }
 slider();
